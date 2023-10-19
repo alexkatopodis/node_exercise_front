cb5ce0b773f1e6f90a37ea89e885d030fae0ecb0
Alex Pereira Katopodis <aperei83@its.jnj.com>
2023-10-19 12:58:23 -0300

    add ajudst style, button message

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Routes from './routes/Routes';
 import { Wrapper, H1, Button } from './style';
+import { Stack } from '@mui/material';
 
 const App = () => {
   return (
     <>
      <Wrapper>
         <H1>PCCW Plataform</H1>
-        <Link to="/users">
-          <Button>Users</Button>
-        </Link>
+          <Stack sx={{ justifyContent: "center" }} className="stack" direction="row" spacing={1}>
+            <Link to="/users">
+              <Button>Users</Button>
+            </Link>
+            <Link to="/messages">
+              <Button>Messages</Button>
+            </Link>
+          </Stack>
         <Routes />
       </Wrapper>
     </>
